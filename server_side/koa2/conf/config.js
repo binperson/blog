7ba82b17aab83e2dfdc1d62a19f1b89d6b7da620
xuxingguo <xuxingguo@longshine.com>
2018-09-13 12:01:08 +0800

    初始化koa2、log4js和orm

---
+++ server_side/koa2/conf/config.js
@@ -0,0 +1,8 @@
+//https://www.cnblogs.com/layezi/p/7290082.html
+//https://www.cnblogs.com/renzhicai/p/7773080.html
+module.exports = {
+  appName: "blog",
+  mongoURL: 'mongodb://119.23.255.183/blog',
+  redisHost: '119.23.255.183',
+  redisPort: '6390',
+}
