b059247148fe869f96a18a4ffdf440598e7e1978
xuxingguo <xuxingguo@longshine.com>
2018-09-26 22:29:06 +0800

    [前端] 完成Footer

--- client_side/vue/src/components/About.vue
+++ client_side/vue/src/components/About.vue
@@ -1,12 +1,18 @@
 <template>
-    <div id='main'>1111
-        <div v-html="html"></div>
+    <div id='main'>
+      <div id='main'>
+      <article class="post page">
+        {{ html }}
+      </article>
+      <my-footer></my-footer>
+    </div>
     </div>
 </template>
 
 <script>
 
 import model from '@/model'
+import MyFooter from '@/components/Footer'
 
 export default {
   data () {
@@ -15,6 +21,9 @@ export default {
         this.html = html
       })
     }
+  },
+  components: {
+    MyFooter
   }
 }
 </script>
